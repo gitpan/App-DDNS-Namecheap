10edc76528387bcc3c88b8a58bc35560892c77e0
David Watson <terminalfool@yahoo.com>
2014-04-10 01:42:28 +0000

    Import of DWATSON/App-DDNS-Namecheap-0.011 from CPAN.
    gitpan-cpan-distribution: App-DDNS-Namecheap
    gitpan-cpan-version:      0.011
    gitpan-cpan-path:         DWATSON/App-DDNS-Namecheap-0.011.tar.gz
    gitpan-cpan-author:       DWATSON
    gitpan-cpan-maturity:     released

--- lib/App/DDNS/Namecheap.pm
+++ lib/App/DDNS/Namecheap.pm
@@ -1,10 +1,9 @@
 package App::DDNS::Namecheap;
 {
-  $App::DDNS::Namecheap::VERSION = '0.01';
+  $App::DDNS::Namecheap::VERSION = '0.011';
 }
-use Moose;
 
-use Carp qw(carp);
+use Moose;
 use LWP::Simple qw($ua get);
 $ua->agent("");
 use Mozilla::CA;
@@ -12,28 +11,29 @@ use Mozilla::CA;
 has domain => ( is => 'ro', isa => 'Str', required => 1 );
 has password => ( is => 'ro', isa => 'Str', required => 1 );
 has hosts => ( is => 'ro', isa => 'ArrayRef', required => 1 );
-has ip => ( is => 'rw', isa => 'Str', required => 0 );
+has ip => ( is => 'rw', isa => 'Str', builder => 'external_ip' );
 
 sub update {
-    my $self = shift;
-
-    $self->{ip} = get("http://checkip.dyndns.org/");
-    if ($self->{ip} =~ /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/) {
-	    $self->{ip} = $1;
-    } else {
-            carp "failed to get external ip";
-	    undef $self->{ip};
+  my $self = shift;
+  unless ( $self->{ip} eq 0 ) {
+    foreach ( @{ $self->{hosts} } ) {
+      my $url = "https://dynamicdns.park-your-domain.com/update?host=$_&domain=$self->{domain}&password=$self->{password}&ip=$self->{ip}";
+      if ( my $return = get($url) ) {
+        $self->log("$0: $return") unless $return =~ /<errcount>0<\/errcount>/is;
+      }
     }
-
-    if ( $self->{ip} ) {
-        foreach ( @{ $self->{hosts} } ) {
-            my $url = "https://dynamicdns.park-your-domain.com/update?host=$_&domain=$self->{domain}&password=$self->{password}&ip=$self->{ip}";
-	    if ( my $return = get($url) ) {
-	        carp "$return" unless $return =~ /<errcount>0<\/errcount>/is;
-	    }
-        }
-    }
+  }
 }
+
+sub external_ip {
+  my $self = shift;
+  my $ip = get("http://icanhazip.com") || 0;
+  $ip = ( $ip =~ /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/ ? $1 : 0 );
+  $self->log("$0: failed to get external ip") if $ip eq 0;
+  return $ip;
+}
+
+sub log { shift; print @_, "\n" }
 
 no Moose;
 
@@ -43,18 +43,19 @@ __END__
 
 =head1 NAME
 
-App::DDNS::Namecheap - Dynamic DNS update for Namecheap registered domains.
+App::DDNS::Namecheap - Dynamic DNS update utility for Namecheap registered domains
 
 =head1 VERSION
 
-version 0.01
+version 0.011
 
 =head1 SYNOPSIS
 
     my $domain =  App::DDNS::Namecheap->new(
                       domain   => 'mysite.org',
          	      password => 'abcdefghijklmnopqrstuvwxyz012345',
-		      hosts    => [ "@", "www" ],
+		      hosts    => [ "@", "www", "*" ],
+		      ip       => '127.0.0.1',      #defaults to external ip
     );
 
     $domain->update();
@@ -63,6 +64,24 @@ version 0.01
 
 This module provides a method for setting the address records of your Namecheap registered 
 domains to your external IP address. 
+
+=head1 METHODS
+
+=over 4
+
+=item B<update>
+
+Updates Namecheap A records using the four attributes listed above.
+
+=item B<external_ip>
+
+Pulls external ip using a public service.
+
+=item B<log>
+
+Logs output to stderr.
+
+=back
 
 =head1 CAVEATS
 
--- scripts/update.pl
+++ scripts/update.pl
@@ -10,9 +10,9 @@ my $timeout = 24;  # 24 hour timeout
 $timeout *= 3600;
 
 my $domain =  App::DDNS::Namecheap->new(
-                  domain   => 'website.com',
+                  domain   => 'mysite.org',
 		  password => 'abcdefghijklmnopqrstuvwxyz012345',
-		  hosts    => [ "@", "www" ],
+		  hosts    => [ "@", "www", "*" ],
 );
 
 while (1) {
@@ -30,7 +30,7 @@ update.pl - command line stub
 
 =head1 DESCRIPTION
 
-Dynamic DNS update daemon for Namecheap registered domains.
+Dynamic DNS update stub for Namecheap registered domains
 
 =head1 CAVEATS
 
